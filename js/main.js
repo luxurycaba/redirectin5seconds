96aeefab8da2792d0e28f1daf7dbf3259d9084d2
belcebu <luxurycaba@gmail.com>
2024-07-13 18:22:32 -0300

    redirect

---
+++ js/main.js
@@ -0,0 +1,18 @@
+        // Configuración de la redirección
+        const redireccionarA = "https://wa.link/montana22";
+        const tiempoTotal = 5; // Tiempo en segundos
+
+        // Inicialización del contador
+        let contador = tiempoTotal;
+        const elementoContador = document.getElementById("contador");
+
+        // Actualización del contador cada segundo
+        const intervalo = setInterval(() => {
+            contador--;
+            elementoContador.textContent = contador;
+
+            if (contador <= 0) {
+                clearInterval(intervalo);
+                window.location.href = redireccionarA;
+            }
+        }, 1000);
